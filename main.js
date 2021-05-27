076bdcbdf318485d146f5001e9bd9bad246d1b0b
Julio Cesar <60150667+jvneto@users.noreply.github.com>
2021-05-27 00:05:36 -0300

    v3.0.4

--- main.js
+++ main.js
@@ -1,26 +1,67 @@
-const { app, BrowserWindow, screen } = require('electron');
-let window;
+const { app, ipcMain, screen } = require('electron');
+var windowManager = require('electron-window-manager');
 
-app.whenReady().then(() => {
+var Load = null;
+
+app.on('ready', function () {
   ScreenSize = screen.getPrimaryDisplay();
   ScreenSize = ScreenSize.bounds;
   // var height = Math.round(ScreenSize.height * 0.6);
   // const width = Math.round((16 * height) / 9);
   var height = Math.round(ScreenSize.height * 0.5);
   const width = Math.round((16 * height) / 11);
-  window = new BrowserWindow({
-    titleBarStyle: 'sBotics Launcher',
-    width: width,
-    height: height,
-    backgroundColor: '#000',
-    titleBarStyle: 'hidden',
-    frame: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-    },
+
+  windowManager.init({
+    devMode: false,
+    defaultWindowTitle: 'sBotics Launcher',
   });
-  window.setMenuBarVisibility(false);
-  window.loadURL('file://' + __dirname + '/routes/login.html');
+  Load = windowManager.createNew(
+    'load',
+    'sBotics Launcher',
+    'file://' + __dirname + '/routes/load.html',
+    false,
+    {
+      width: width,
+      height: height,
+      showDevTools: true,
+      DevTools: true,
+      menu: null,
+      transparent: false,
+      frame: false,
+      resizable: true,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true,
+      },
+    },
+  );
+  Load.open();
 });
+
+// const { app, BrowserWindow, screen } = require('electron');
+// let window;
+
+// app.whenReady().then(() => {
+//   ScreenSize = screen.getPrimaryDisplay();
+//   ScreenSize = ScreenSize.bounds;
+//   // var height = Math.round(ScreenSize.height * 0.6);
+//   // const width = Math.round((16 * height) / 9);
+//   var height = Math.round(ScreenSize.height * 0.5);
+//   const width = Math.round((16 * height) / 11);
+//   window = new BrowserWindow({
+//     titleBarStyle: 'sBotics Launcher',
+//     width: width,
+//     height: height,
+//     backgroundColor: '#000',
+//     titleBarStyle: 'hidden',
+//     frame: false,
+//     webPreferences: {
+//       nodeIntegration: true,
+//       contextIsolation: false,
+//       enableRemoteModule: true,
+//     },
+//   });
+//   window.setMenuBarVisibility(false);
+//   window.loadURL('file://' + __dirname + '/routes/load.html');
+// });
