c61d989d72f2be23deb1d0be41ce875e5120b105
Julio Cesar Vera Neto <neto-vera@hotmail.com>
2021-06-24 03:20:45 -0300

    v3.1.10

--- src/class/__index_manager.js
+++ src/class/__index_manager.js
@@ -32,6 +32,23 @@ import {
   ReportDownloadButton,
 } from '../utils/relatorio-download-manager.js';
 
+// for (let index = 0; index < 5; index++) {
+//   Toastify({
+//     avatar:
+//       'https://pickaface.net/gallery/avatar/unr_teste_180803_0038_2l6fzsr.png',
+//     text: 'This is a toast',
+//     duration: 3000,
+//     destination: 'https://github.com/apvarun/toastify-js',
+//     newWindow: true,
+//     close: true,
+//     gravity: 'top', // `top` or `bottom`
+//     position: 'right', // `left`, `center` or `right`
+//     backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+//     stopOnFocus: true, // Prevents dismissing of toast on hover
+//     onClick: function () {}, // Callback after click,
+//   }).showToast();
+// }
+
 // Interface Manager
 $('.close-alert').click(() => {
   $('.top-alert').css('display', 'none');
