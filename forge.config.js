d75e0e0d0d37e488bfff7e2467b3433506e13750
Julio Cesar Vera Neto <neto-vera@hotmailk.com>
2022-01-23 22:45:09 -0300

    fix

---
+++ forge.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const iconDir = path.resolve(__dirname, 'assets', 'icons', 'app');
+
+const commonLinuxConfig = {
+    icon: {
+        scalable: path.resolve(iconDir, 'icon.svg')
+    },
+    mimeType: ['x-scheme-handler/sbotics']
+};
+
+const config = {
+    packagerConfig: {
+        asar: true,
+        appBundleId: 'com.sbotics.launcher',
+        usageDescription: [],
+        appCategoryType: 'public.app-category.developer-tools',
+        protocols: [{
+            name: 'sBotics Launcher Launch Protocol',
+            schemes: ['sbotics']
+        }],
+        win32metadata: {
+            CompanyName: 'sBotics',
+            OriginalFilename: 'sBotics'
+        }
+    },
+    makers: [{
+            name: '@electron-forge/maker-squirrel',
+            platforms: ['win32'],
+            arch: ['ia32', 'x64'],
+            config: {
+                name: 'sBotics',
+                authors: 'sBotics',
+                exe: 'sbotics.exe',
+                iconUrl: path.resolve(iconDir, 'icon.ico'),
+                noMsi: true,
+                setupExe: 'sbotics-setup.exe',
+                setupIcon: path.resolve(iconDir, 'icon.ico')
+            },
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            platforms: ['linux'],
+            config: commonLinuxConfig
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            platforms: ['linux'],
+            config: commonLinuxConfig
+        }, {
+            name: '@electron-forge/maker-pkg',
+            platforms: ['darwin'],
+            arch: 'all',
+            config: {
+                icon: '/assets/icons/app/icon.icns',
+                install: '/Applications'
+            }
+        }
+    ],
+    publishers: [{
+        name: '@electron-forge/publisher-github',
+        config: {
+            repository: {
+                owner: 'sbotics',
+                name: 'launcher',
+            },
+            draft: true,
+            prerelease: false
+        }
+    }]
+};
+
+module.exports = config;
