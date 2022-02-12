3a529106ee4d0700ceb329a8a5a033e50e8e3fbc
Julio Cesar Vera Neto <neto-vera@hotmailk.com>
2022-02-12 01:09:18 -0300

    Login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    content: ["./routes/*.html", "./src/**/*.js"],
-    theme: {
-        colors: {
-            sbotics_gray: {
-                100: "#f4EEF2",
-                400: "#8E8E93",
-                900: "#252425",
-            },
-            red: colors.red,
-            green: colors.green,
-            black: colors.black,
-            gray: colors.gray,
-        },
-        fontFamily: {
-            sans: ['sans-serif'],
-        },
-        extend: {},
+  content: ['./routes/*.html', './src/**/*.js'],
+  theme: {
+    colors: {
+      sbotics_gray: {
+        100: '#f4EEF2',
+        400: '#8E8E93',
+        900: '#252425',
+      },
+      red: colors.red,
+      green: colors.green,
+      black: colors.black,
+      gray: colors.gray,
+      white: colors.white,
     },
-    plugins: [],
-}
+    fontFamily: {
+      sans: ['sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
