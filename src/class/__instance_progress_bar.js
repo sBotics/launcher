9288a8a4f6b41e503fe3d2afb3e06431d0131032
Julio Cesar Vera Neto <neto-vera@hotmail.com>
2022-05-01 02:19:14 -0300

    Fix

--- src/class/__instance_progress_bar.js
+++ src/class/__instance_progress_bar.js
@@ -5,21 +5,30 @@ class ProgressBar {
   defaultState() {
     return {
       warning: {
-        bg: 'bg-orange--',
+        bg: 'bg-orange-500',
       },
       danger: {
-        bg: 'bg-red--',
+        bg: 'bg-red-500',
       },
       success: {
-        bg: 'bg-green--',
+        bg: 'bg-green-500',
       },
       info: {
-        bg: 'bg-blue--',
+        bg: 'bg-blue-500',
       },
     };
   }
   clear() {
     document.getElementById('__container_progress_bar').innerHTML = '';
+  }
+  styleHeight(height = 'default') {
+    const heights = {
+      small: '10px',
+      default: '15px',
+      large: '35px',
+    };
+    document.getElementById('__container_progress_bar').style.height =
+      heights[height];
   }
   activate(options) {
     if (!options) options = { clear: false };
@@ -45,17 +54,49 @@ class ProgressBar {
         state: '',
         id: '',
         prefix: '__sBotics_Download',
+        grid: false,
       },
       options,
     );
 
-    document.getElementById(
-      options.progressBarContainer,
-    ).innerHTML += `<div id="${options.prefix}_${
+    document.getElementById(options.progressBarContainer).innerHTML += `${
+      options.grid
+        ? '<div class="w-full h-full flex justify-start items-end border-[1px] border-white">'
+        : ''
+    } <div id="${options.prefix}_${
       options.id
     }" class="w-full block transition-height duration-[2000ms] ease-in-out ${
       options.state ? this.defaultState()[options.state].bg : ''
-    }" style="height: ${options.percentage > 100 ? 100 : options.percentage}%;"></div>`;
+    }" style="height: ${
+      options.percentage > 100 ? 100 : options.percentage
+    }%;"></div>${options.grid ? '</div>' : ''}`;
+  }
+  update(options) {
+    options = extend(
+      {
+        percentage: '0',
+        state: '',
+        id: '',
+        prefix: '__sBotics_Download',
+      },
+      options,
+    );
+
+    const processProgressBar = document.getElementById(
+      `${options.prefix}_${options.id}`,
+    );
+
+    processProgressBar.classList.remove(`${this.defaultState()['warning'].bg}`);
+    processProgressBar.classList.remove(`${this.defaultState()['danger'].bg}`);
+    processProgressBar.classList.remove(`${this.defaultState()['success'].bg}`);
+    processProgressBar.classList.remove(`${this.defaultState()['info'].bg}`);
+    processProgressBar.classList.add(
+      `${options.state ? this.defaultState()[options.state].bg : ''}`,
+    );
+
+    processProgressBar.style.height = `${
+      options.percentage > 100 ? 100 : options.percentage
+    }%`;
   }
 }
 
--- src/controllers/__main_download.js
+++ src/controllers/__main_download.js
@@ -1,7 +1,7 @@
-var extend = require('extend-shallow');
-var hljs = require('highlight.js');
+let extend = require('extend-shallow');
+let hljs = require('highlight.js');
 let swal = require('sweetalert2');
-var markdown = require('markdown-it')({
+let markdown = require('markdown-it')({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
@@ -21,25 +21,275 @@ var markdown = require('markdown-it')({
   },
 });
 
+import { Alerts } from '../class/__instance_alerts.js';
 import { Application } from '../class/__instance_application.js';
 import { Connection } from '../class/__instance_connection.js';
 import { MagicButton } from '../class/__instance_magic_button.js';
 import { Exception } from '../class/__instance_exception.js';
 import { FileConfig } from '../class/__instance_file_config.js';
-import { FindSync } from '../utils/files-manager.js';
+import { FileSizeSync, FindSync } from '../utils/files-manager.js';
+import { ProgressBar } from '../class/__instance_progress_bar.js';
 
+let alert = new Alerts();
 let connection = new Connection();
 let application = new Application();
 let fileConfig = new FileConfig();
+let progressBar = new ProgressBar();
 
+let dataContent = null;
 
-function MagicButtonAction(action) {
-  switch (action) {
-    case 'install':
+const blackList = []; // Indica para o verificador, ignorar os caminhos contidos dentro dessa lista
+
+// ===========================
+
+const FailDownload = (options) => {
+  options = extend(
+    {
+      mode: 0,
+      message: '',
+    },
+    options,
+  );
+  const message = options.message;
+
+  switch (options.mode) {
+    case 1:
+      alert.createTop({
+        states: 'danger',
+        message: message,
+      });
+      progressBar.clear();
+      new MagicButton({
+        mode: 'fail',
+        text: message,
+      });
       break;
 
     default:
+      alert.createTop({
+        states: 'danger',
+        message: `${message} - Se a falha persistir, entre em contato com os desenvolvedores`,
+        fixed: true,
+        timeOutVIew: 0,
+      });
+      progressBar.clear();
+      progressBar.styleHeight('small');
+      progressBar.create({
+        id: '1',
+        state: 'danger',
+        percentage: 100,
+      });
+      new MagicButton({
+        mode: 'fail',
+        text: message,
+      });
       break;
+  }
+};
+
+function CheckFile(options) {
+  options = extend(
+    {
+      path: '',
+      name: '',
+      size: '',
+      lastUpdatedAt: '',
+      forceInstall: false,
+    },
+    options,
+  );
+
+  const path = options.path;
+  const name = options.name;
+  const size = options.size;
+  const lastUpdatedAt = options.lastUpdatedAt;
+  const pathDownload = `sBotics/${path + name}`;
+  const forceInstall = options.forceInstall;
+
+  if (forceInstall) {
+    if (FileSizeSync(pathDownload).size != size) {
+      if (BlackList.indexOf(pathDownload) > -1) return true;
+      else return false;
+    }
+    return false;
+  }
+
+  if (FindSync(pathDownload)) {
+    const donwloadFileTime = ParseTime(lastUpdatedAt);
+    const saveFileTime = Math.floor(FileSizeSync(pathDownload).mtimeMs);
+    if (donwloadFileTime >= saveFileTime) {
+      return BlackList.indexOf(pathDownload) > -1;
+    } else if (FileSizeSync(pathDownload).size != size) {
+      return BlackList.indexOf(pathDownload) > -1;
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function CheckFileUpdate(options) {
+  options = extend(
+    {
+      dataContent: '',
+    },
+    options,
+  );
+  try {
+    const dataContent = JSON.parse(options.dataContent);
+
+    // localStorage.setItem('versionSbotics', dataUpdate['version']);
+
+    // MODO DO FORCE INSTALL QUE VEM DIRETAMENTE DO ARQUIVO
+    // const forceInstall = ForceInstallManager({
+    //   forceInstall: dataUpdate['force'],
+    //   newSboticsVersion: dataUpdate['version'],
+    // });
+
+    progressBar.clear();
+    progressBar.styleHeight('large');
+
+    let filesID = dataContent['data'].length + 1;
+    let filesFind = 0;
+    let filesNotFind = 0;
+
+    dataContent['data'].map((dataContent) => {
+      const fileID = --filesID;
+
+      progressBar.create({
+        percentage: 5,
+        id: fileID,
+        state: 'info',
+        grid: false,
+      });
+
+      //   progressBar.update({
+      //     percentage: 100,
+      //     id: fileID,
+      //     state: 'success',
+      //   });
+
+      if (
+        CheckUpdate({
+          path: dataUpdate.path,
+          name: dataUpdate.name,
+          size: dataUpdate.size,
+          lastUpdatedAt: dataUpdate.last_updated_at,
+          forceInstall: forceInstall,
+        })
+      ) {
+        Update({
+          id: fileID,
+          addState: 'sbotics-okfiles',
+          removeState: 'info',
+        });
+        filesFind = filesFind + 1;
+      } else {
+        Update({
+          id: fileID,
+          addState: 'warning',
+          removeState: 'info',
+        });
+        filesNotFind = filesNotFind + 1;
+      }
+    });
+    // return {
+    //   filesFind: filesFind,
+    //   filesNotFind: filesNotFind,
+    //   dataUpdateFiles: dataUpdateFilesSize,
+    //   forceInstall: forceInstall,
+    // };
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function FilesVerification(options) {
+  progressBar.clear();
+
+  options = extend(
+    {
+      modeText: '',
+      autoOpen: false,
+      autoInstall: false,
+      installCheck: false,
+      typeModel: '',
+    },
+    options,
+  );
+
+  const modeText = options.modeText;
+  const autoOpen = options.autoOpen;
+  const autoInstall = options.autoInstall;
+  const installCheck = options.installCheck;
+  const typeModel = options.typeModel;
+
+  new MagicButton({
+    mode: 'process',
+    text: modeText,
+  });
+
+  if (!dataContent) {
+    return FailDownload({
+      message: 'Fracasso! Ao procurar por atualização do sBotics',
+    });
+  }
+
+  const checkFileUpdate = CheckFileUpdate({ dataContent: dataContent });
+
+  if (!checkFileUpdate) {
+    return FailDownload({
+      message: 'Fracasso! Ao verificar arquivos sBotics.',
+    });
+  }
+
+  const filesFind = checkFileUpdate.filesFind;
+  const dataUpdateFiles = checkFileUpdate.dataUpdateFiles;
+
+  if (filesFind == dataUpdateFiles) {
+    if (autoOpen) {
+      application.OpenSbotics();
+    } else {
+      new MagicButton({
+        mode: 'start',
+      });
+
+      if (installCheck) {
+        switch (typeModel) {
+          case 'install':
+            alert.createTop({
+              states: 'success',
+              message: `${message} - sBotics instalado com sucesso! Pronto para abrir.`,
+            });
+            break;
+          case 'update':
+            alert.createTop({
+              states: 'success',
+              message: `${message} - sBotics atualizado com sucesso! Pronto para abrir.`,
+            });
+            break;
+        }
+      }
+    }
+  } else {
+    if (autoInstall)
+      DownloadsBotics({
+        modeText: Lang('Installing sBotics! Please wait...'),
+        type: 'install',
+      });
+    else {
+      if (filesFind > 0) {
+        MagicButton({
+          mode: 'update',
+        });
+      } else {
+        new MagicButton({
+          mode: 'install',
+        });
+      }
+    }
   }
 }
 
@@ -86,6 +336,10 @@ function start(options) {
 }
 
 export async function LoadingDownloadController() {
+  progressBar.activate();
+  progressBar.styleHeight('default');
+  progressBar.clear();
+
   new MagicButton({
     mode: 'process',
     text: 'Procurando Atualização...',
@@ -96,6 +350,7 @@ export async function LoadingDownloadController() {
     .then(function (response) {
       try {
         const data = response.data;
+        dataContent = data.content;
         start({
           name: data.name,
           type: data.type,
@@ -134,11 +389,13 @@ export async function LoadingDownloadController() {
     });
 }
 
-window.OpenInstallFolder = function OpenInstallFolder(){
+window.OpenInstallFolder = function OpenInstallFolder() {
   if (FindSync('sBotics/')) {
     application.openInstallFolder();
   } else {
-    // FilesVerification({ modeText: Lang('Looking for update! Please wait...') });
+    FilesVerification({
+      modeText: 'Procurando atualização! Por favor, espere...',
+    });
     swal
       .fire({
         icon: 'error',
@@ -156,5 +413,15 @@ window.OpenInstallFolder = function OpenInstallFolder(){
           // });
         }
       });
+  }
+};
+
+window.MagicButtonAction = function MagicButtonAction(action) {
+  switch (action) {
+    case 'install':
+      break;
+
+    default:
+      break;
   }
 };
